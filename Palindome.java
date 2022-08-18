73f43417f684639689460d08ebb827bcbad7fe58
Goswami <Niket.Goswami@emtecinc.com>
2022-08-18 11:35:50 +0530

    First Commit

--- Palindome.java
+++ Palindome.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Palindome {
+public class Palindome {
+    public static void main(String args[]){
+        int r,sum=0,temp;
+        int n=453;
+
+        temp=n;
+        while(n>0){
+            r=n%10;
+            sum=(sum*10)+r;
+            n=n/10;
+        }
+        if(temp==sum)
+            System.out.println("palindrome number ");
+        else
+            System.out.println("not palindrome");
+    }
 }
