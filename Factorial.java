b873df82ed428de178e1e17c93ed39bdb213cf93
Goswami <Niket.Goswami@emtecinc.com>
2022-08-24 10:19:11 +0530

    New Branch

--- Factorial.java
+++ Factorial.java
@@ -1,7 +1,7 @@
 public class Factorial {
     public static void main(String[] args) {
 
-        int num = 10;
+        int num = 25;
         long factorial = 1;
         for(int i = 1; i <= num; ++i)
         {
